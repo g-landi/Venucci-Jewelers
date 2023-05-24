3fecae4d862a8570b736d8746abe797d2abbb77b
Gael-Landi <gael.landi4212@gmail.com>
2023-05-24 17:13:35 -0400

    Test build

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { app, BrowserWindow, screen, nativeImage, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const path = require('path');
